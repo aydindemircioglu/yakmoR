6ef3d4e5f88efbd7a970d85b09c18f53b8d8795c
aydin demircioglu <githubAD@cloned.de>
2015-06-18 15:10:14 +0200

    win32 fix try

--- R/zzz.R
+++ R/zzz.R
@@ -23,28 +23,28 @@
 #' @useDynLib yakmoR
 #' @importFrom Rcpp sourceCpp
 
- 
+
 # create own environment
 yakmoREnv = new.env(parent = emptyenv())
 
 
- .onLoad <- function(libname, pkgname) {
-  op <- options()
-  op.devtools <- list(
-    devtools.path = "~/R-dev",
-    devtools.install.args = "",
-    devtools.name = "Aydin Demircioglu",
-    devtools.desc.author = '"Aydin Demircioglu <aydin.demircioglu@ini.rub.de> [aut, cre]"',
-    devtools.desc.license = "LGPL-3 + file LICENSE",
-    devtools.desc.suggests = NULL,
-    devtools.desc = list()
-  )
-  toset <- !(names(op.devtools) %in% names(op))
-  if(any(toset)) options(op.devtools[toset])
-  
-#  find_rtools()
-  
-  invisible()
+.onLoad <- function(libname, pkgname) {
+	op <- options()
+	op.devtools <- list(
+		devtools.path = "~/R-dev",
+		devtools.install.args = "",
+		devtools.name = "Aydin Demircioglu",
+		devtools.desc.author = '"Aydin Demircioglu <aydin.demircioglu@ini.rub.de> [aut, cre]"',
+		devtools.desc.license = "LGPL-3 + file LICENSE",
+		devtools.desc.suggests = NULL,
+		devtools.desc = list()
+	)
+	toset <- !(names(op.devtools) %in% names(op))
+	if(any(toset)) options(op.devtools[toset])
+
+	#  find_rtools()
+
+	invisible()
 }
 
 
@@ -54,5 +54,5 @@ yakmoREnv = new.env(parent = emptyenv())
 
 
 .onUnload <- function (libpath) {
-  library.dynam.unload("yakmoR", libpath)
+	library.dynam.unload("yakmoR", libpath)
 }
